07a673d312b0ba8e946b3a1baec6440d4f5c113d
Ammon Morris <likesalmon@gmail.com>
2017-07-11 19:50:27 -0700

    fix(app/services/github/index.js): correct makeRequest import

--- app/services/github/index.js
+++ app/services/github/index.js
@@ -6,4 +6,4 @@
 
 // The makeRequest() constitutes the external API for this service.
 // Calling this function initiates a request.
-export makeRequest from './actions';
+export { makeRequest } from './actions';
