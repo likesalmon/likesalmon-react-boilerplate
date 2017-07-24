34b0c5694735bfd6f90faf54b87bc66e16958e07
Julie Petrarca <jpetrarca@apexclearing.com>
2017-07-24 12:42:35 -0700

    feat(Service Generator): registers serviceGenerator with generator list; adds handlebars 'lowercase' helper

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -10,19 +10,29 @@ const componentGenerator = require('./component/index.js');
 const containerGenerator = require('./container/index.js');
 const routeGenerator = require('./route/index.js');
 const languageGenerator = require('./language/index.js');
+const serviceGenerator = require('./service/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
   plop.setGenerator('container', containerGenerator);
   plop.setGenerator('route', routeGenerator);
   plop.setGenerator('language', languageGenerator);
+  plop.setGenerator('service', serviceGenerator);
   plop.addHelper('directory', (comp) => {
     try {
-      fs.accessSync(path.join(__dirname, `../../app/containers/${comp}`), fs.F_OK);
+      fs.accessSync(
+        path.join(__dirname, `../../app/containers/${comp}`),
+        fs.F_OK
+      );
       return `containers/${comp}`;
     } catch (e) {
       return `components/${comp}`;
     }
   });
   plop.addHelper('curly', (object, open) => (open ? '{' : '}'));
+  plop.addHelper(
+    'lowercase',
+    (string) =>
+      (string && typeof string === 'string' ? string.toLowerCase() : string)
+  );
 };
